0e1f726c6ea2f9fcb2a43a17a960a1fc858275e0
minodisk <daisuke.mino@gmail.com>
2016-04-14 23:48:40 +0900

    Add halftone filter

--- release.sh
+++ release.sh
@@ -2,20 +2,22 @@
 
 set -e
 
-npm run build
 version=$(npm version ${1:-"patch"})
+npm run build
 # git commit -a -m "Bump version to v$version"
 git push
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-  --pack-extension=dist \
-  --pack-extension-key=crx.pem
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+#   --pack-extension=dist \
+#   --pack-extension-key=crx.pem
+zip dist
 rm -rf dist
-mv dist.crx sqwilter.crx
+# mv dist.crx sqwilter.crx
+mv dist.zip sqwilter.zip
 
-hub release create -a sqwilter.crx -m "Build: $version" "$version"
-rm -rf sqwilter.crx
-npm run updates
-
-git commit -a -m "Update crx URL to $version"
-git push
+# hub release create -a sqwilter.crx -m "Build: $version" "$version"
+# rm -rf sqwilter.crx
+# npm run updates
+#
+# git commit -a -m "Update crx URL to $version"
+# git push
